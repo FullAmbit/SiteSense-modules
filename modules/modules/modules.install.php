e749a370c7290fe918c06e15873fa994427b324b
Zach Bloomquist <zbloomquist@fullambit.net>
2012-09-16 15:31:27 -0400

    Added version 1.0.2

--- modules/modules/modules.install.php
+++ modules/modules/modules.install.php
@@ -26,7 +26,7 @@ function modules_settings() {
 	return array(
 		'name'      => 'modules',
 		'shortName' => 'modules',
-		'version'   => '1.0.1'
+		'version'   => '1.0.2'
 	);
 }
 function modules_install($db,$drop=false,$firstInstall=false,$lang="en_us") {
